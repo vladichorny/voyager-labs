da3dd647e55e5c78c75f6bb9990a0e62c4426d17
vladichorny <vladimir.chorny@gmail.com>
2021-07-25 23:06:20 +0300

    Delete docker_build.sh

--- docker_build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export DOCKER_BUILDKIT=0
-export COMPOSE_DOCKER_CLI_BUILD=0
-cd ${WORKSPACE}/docker-files/docker-service
-
-docker image inspect --format '{{index .RepoTags}}' "docker-service:1.0.0" 2> /dev/null | grep -q "docker-service:1.0.0" && \
-    docker rmi --force docker-service:1.0.0 || true
-docker build --rm --tag docker-service:1.0.0 .
