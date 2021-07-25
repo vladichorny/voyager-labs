e4469f62e380d75a669543af2a91ac92e79ee581
vladichorny <vladimir.chorny@gmail.com>
2021-07-25 17:42:01 +0300

    Add files via upload

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,4 @@
-def docker_service = "docker-service:1.0.0"
-def docker_service_file_name = docker_service.replace(':', '_')
-def publish_content = "output"
+
 cygwin_path = "C:\\cygwin64"
 
 pipeline {
--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker image inspect --format '{{index .RepoTags}}' "${docker_service}" 2> /dev/null | grep -q "${docker_service}" && \
-    docker rmi --force ${docker_service} || true
-docker build --rm --tag ${docker_service} .
+docker image inspect --format '{{index .RepoTags}}' "docker-service:1.0.0" 2> /dev/null | grep -q "docker-service:1.0.0" && \
+    docker rmi --force docker-service:1.0.0 || true
+docker build --rm --tag docker-service:1.0.0 .
